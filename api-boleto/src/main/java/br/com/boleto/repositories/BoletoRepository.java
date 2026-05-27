f2957ceadbcb1231d84bf0926e4d04dbc0dbb723
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-26 22:14:20 -0300

    feat: Cria Repository de Boleto

---
+++ api-boleto/src/main/java/br/com/boleto/repositories/BoletoRepository.java
@@ -0,0 +1,11 @@
+package br.com.boleto.repositories;
+
+import br.com.boleto.entities.Boleto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoletoRepository extends JpaRepository<Boleto, Long> {
+
+    public boolean existsByCodigoBarras(String codigoBarras);
+}
