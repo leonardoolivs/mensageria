fc15a41028bcbbe366044c0fa42cb87655acec3c
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-06-01 19:25:19 -0300

    feat: Cria Consumer de Boleto

---
+++ validador-boleto/src/main/java/br/com/validador/kafka/BoletoConsumer.java
@@ -0,0 +1,18 @@
+package br.com.validador.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoletoConsumer {
+
+    public static final Logger logger = LoggerFactory.getLogger(BoletoConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topico-boleto}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consomeBoleto(BoletoAvro boleto){
+        logger.info("Consumindo mensagem -> %s", boleto);
+    }
+
+}
